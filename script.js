288ad264a9f4d01c15cfa2b5550296b607ff5d1a
Shahed1998 <shahedchowdhury120@gmail.com>
2021-01-06 14:23:06 +0600

    4th commit

--- script.js
+++ script.js
@@ -2,12 +2,28 @@
 let str = '';
 const nodeList = document.querySelectorAll('.btn');
 const screenShow = document.querySelector('#screen');
+const reset = document.querySelector('#reset');
+const deleteBtn = document.querySelector('#delete');
 
+// Making the numbers work and showing onto the screen
 for (const [i, val] of nodeList.entries()) {
   nodeList[i].addEventListener('click', function () {
-    // console.log(nodeList[i].textContent);
     str += nodeList[i].textContent;
-    console.log(str);
     screenShow.textContent = str;
+    console.log(str);
   });
 }
+
+// Reset Button
+reset.addEventListener('click', function () {
+  str = '';
+  console.log(str);
+  screenShow.textContent = str;
+});
+
+// Delete button
+deleteBtn.addEventListener('click', function () {
+  str = str.slice(0, str.length - 1);
+  screenShow.textContent = str;
+  console.log(str);
+});
